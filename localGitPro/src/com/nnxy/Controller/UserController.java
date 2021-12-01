045b65a6f39094b27e103bcd6d46601adb228015
dhk345 <94914613+dhk345@users.noreply.github.com>
2021-12-01 22:44:32 +0800

    Update UserController.java

--- localGitPro/src/com/nnxy/Controller/UserController.java
+++ localGitPro/src/com/nnxy/Controller/UserController.java
@@ -2,4 +2,9 @@ package com.nnxy.Controller;
 
 public class UserController {
 
+      private int id;
+      private String username;
+      private String password;
+      private int sex;
+      private String love;
 }
